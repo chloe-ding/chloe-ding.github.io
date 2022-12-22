6d9053968cad472f55335e1b9265b82999802274
kern.ding <gf4t47@gmail.com>
2022-12-21 18:40:22 -0800

    Initial commit

---
+++ src/ShoppingList.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ header ? header.toLocaleUpperCase() : "Welcome chloe" }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add an item">
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">High Priority
+  </label>
+  <br>
+  {{ newItem }} {{ newItemHighPriority }}
+  <ul>
+    <li v-for="({id, label}, index) in items" :key="id">{{ index }}: {{ label }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ShoppingList.vue",
+  data() {
+    return {
+      header: 'Shopping List App',
+      newItem: '',
+      newItemHighPriority: false,
+      items: [
+        {id: 1, label: "10 bracelets"},
+        {id: 2, label: "1 box of fruit punch"},
+        {id: 3, label: "20 goody bags"}
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import shoppingList from './ShoppingList.vue'
+
+import './assets/main.css'
+
+createApp(shoppingList).mount('#app')
